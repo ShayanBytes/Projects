0f3ee753bf15a66cee9e7212247471e9a2c4155d
ShayanBytes <shayanbytes@gmail.com>
2026-06-14 11:12:47 +0530

    small update

--- Mobile_App/mobile/app/(auth)/index.jsx
+++ Mobile_App/mobile/app/(auth)/index.jsx
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from "../../assets/styles/login.styles";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {}
   return (
-    <View>
-      <Text>login Screen </Text>
+    <View style ={styles.container}>
+    
     </View>
   )
 }
